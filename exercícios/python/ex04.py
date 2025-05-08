9031fb88122043cfb3df3a170639b0cadb830677
Ana Rita <anaritavieeira@gmail.com>
2025-05-08 10:14:49 -0300

    Create ex04.py

---
+++ exercícios/python/ex04.py
@@ -0,0 +1,15 @@
+nome = input("digite o seu nome: ")
+
+while True:
+    try:
+        ano = int(input("digite o ano do seu nascimento (entre 1922 e 2024): "))
+        if ano >= 1922 and ano <= 2024:
+            break
+        else:
+            print("ano fora do intervalo permitido")
+    except:
+        print("entrada inválida. por favor, digite um número")
+
+ano_atual = 2025
+idade = ano_atual - ano
+print(f"{nome}, você vai ter {idade} anos em {ano_atual}.")
