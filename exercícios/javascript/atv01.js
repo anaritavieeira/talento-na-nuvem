de759ac15c279092ad7ee071a7689e6a40261ec8
Ana Rita <anaritavieeira@gmail.com>
2025-06-28 10:43:16 -0300

    novo commit feito

---
+++ exercícios/javascript/atv01.js
@@ -0,0 +1,9 @@
+// laço de repetição com for
+
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0) {
+    console.log(i);
+    }
+
+}
+
---
+++ exercícios/javascript/atv02.js
@@ -0,0 +1,9 @@
+// treinando arrays
+
+const frutas = ['Morango', 'Amora', 'Framboesa', 'Uva'];
+
+console.log(frutas);
+
+frutas.forEach(function(fruta) {
+    console.log(fruta);
+});
